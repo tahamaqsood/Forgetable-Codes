236c52e3f310cede7332fc9ebc173597d51026fc
Taha Maqsood Ali <81623971+tahamaqsood@users.noreply.github.com>
2023-04-17 02:21:22 +0500

    Update config.php

--- Elements stripe/config.php
+++ Elements stripe/config.php
@@ -1,8 +1,8 @@
 <?php
 require_once('vendor/autoload.php');
 
-$publishable_key = "pk_test_51MEe3aFcGkT8Xof2STqDU649YkK4o8gEKrEfG5ingHXOhwn2GRxXsMEMephMeJvlzyGLw4koOiqzRw8ww6tGnOkO00ThFpARtO";
-$secret_key = "sk_test_51MEe3aFcGkT8Xof2vPFBQnWF6JaqWodUQCVazKbybXemsPd91v8ESwdgoDZQ3zbjQcdUMPHjsl7Taezpl4ZjeOUt00yv8KLJOd";
+$publishable_key = "TEST_KEY";
+$secret_key = "TEST_KEY";
 
 
 \Stripe\Stripe::setApiKey($secret_key);
